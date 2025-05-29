cbdbcef855f4a92b2879f1f26a2b57ee494034f9
Hacer Yavas <haceryavaas@gmail.com>
2025-05-29 17:17:03 +0300

    artik_yil

---
+++ src/ArtikYil.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * @author Hacer Nur YAVAŞ - 29 Mayıs 2025
+ * Bu proje girilen yıla göre artık yıl olup olmadığını bulur.
+ */
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int yil;
+        boolean isArtik = false;
+
+        System.out.print("Yıl giriniz: ");
+        yil = scanner.nextInt();
+
+        if (((yil % 4 == 0) && (yil % 100 != 0)) || (yil % 400 == 0))
+            isArtik = true;
+        else
+            isArtik = false;
+
+        if (isArtik)
+            System.out.println(yil + " bir artik yıldır.");
+        else
+            System.out.println(yil + " bir artik yıl değildir.");
+    }
+}
