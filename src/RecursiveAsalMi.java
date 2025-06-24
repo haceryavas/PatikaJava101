f5391ab4f337f6651c19a69c194d3003b579d99a
Hacer Yavas <haceryavaas@gmail.com>
2025-06-25 00:22:24 +0300

    versionnn

--- src/RecursiveAsalMi.java
+++ src/RecursiveAsalMi.java
@@ -1,25 +1,43 @@
 import java.util.Scanner;
+
 /**
  * @author Hacer Nur YAVAŞ - 24 Haziran 2025
  * Bu proje kullanıcıdan aldığı sayının asal olup olmadığını "Recursive" metot kullanarak kontrol eder.
  */
 public class RecursiveAsalMi {
+
+    static boolean isPrime(int number, int divisor) {
+
+//        if (number <= 2)
+//            return number == 2;
+//        if (number != divisor){
+//            if (number % divisor == 0)
+//                return false;
+//            else
+//                return isPrime(number, divisor + 1);
+//        }
+//        else
+//            return true;
+
+        if (number <= 2)
+            return number == 2;
+        if (number % divisor == 0)
+            return false;
+        if (divisor * divisor > number)
+            return true;
+        return isPrime(number, divisor + 1);
+    }
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Sayı giriniz: ");
         int number = scan.nextInt();
 
-        boolean isAsal;
+        if (isPrime(number, 2))
+            System.out.println(number + " sayısı bir asal sayıdır.");
+        else
+            System.out.println(number + " sayısı bir asal sayı değildir.");
 
-        for (int i = 1; i <= 100; i++) {
-            isAsal = false;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isAsal = true;
-                }
-            }
-            if (!isAsal && i != 1)
-                System.out.print(i + " ");
-        }
     }
 }
